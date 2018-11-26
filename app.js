651c2069fd2581b87a51237b979276c13e94f32b
guogaocheng <guogaocheng@zuinianqing.com>
2018-11-26 16:01:20 +0800

    update

--- app.js
+++ app.js
@@ -16,7 +16,7 @@ app.use(bodyParser.json());
 app.use(bodyParser.urlencoded({ extended: false }));
 app.use(cookieParser());
 app.use(express.static(path.join(__dirname, 'public')));
-app.use('/angular', express.static(__dirname + '/node_modules/angular'));
+// app.use('/angular', express.static(__dirname + '/node_modules/angular'));
 
 app.use('/users', users);
 
--- conf/mysqlConf.js
+++ conf/mysqlConf.js
@@ -2,8 +2,8 @@ module.exports = {
     mysql: {
         host: 'localhost',
         user: 'root',
-        password: '978299',
-        database:'express-mysql-demo',
+        password: 'MyNewPass4!',
+        database:'user',
         // 最大连接数，默认为10
         connectionLimit: 10
     }
